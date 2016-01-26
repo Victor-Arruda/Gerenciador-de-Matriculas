000c03ffce9831e52932bd51416dcb7987129516
Victor Arruda <victor.arruda23@gmail.com>
2016-01-26 16:55:21 -0200

    Outros ajustes nas views

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -14,6 +14,7 @@ class EnrollmentsController < ApplicationController
 
   # GET /enrollments/new
   def new
+    @years = [['2010', 2010],['2011', 2011],['2012', 2012],['2013', 2013],['2014', 2014],['2015', 2015],['2016', 2016],['2017', 2018],['2019', 2019],['2020', 2020]]
     @enrollment = Enrollment.new
   end
 
